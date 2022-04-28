6f88c536a259b2fc2642847d1b006c3834a3b915
Waranyu Khongchai <photboatxeemus@icloud.com>
2022-04-28 14:38:23 +0700

    clean code project

---
+++ Project.TodoList/CoreData/Entry+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entry+CoreDataClass.swift
+//  
+//
+//  Created by Waranyu Khongchai on 21/4/2565 BE.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Entry)
+public class Entry: NSManagedObject {
+
+}
